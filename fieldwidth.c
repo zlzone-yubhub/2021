bdc69d44f524446d1d02e280632e3ea98c8a6835
zlzone-yubhub <76840441+zlzone-yubhub@users.noreply.github.com>
2021-01-09 07:34:42 +0900

    Add files via upload

---
+++ fieldwidth.c
@@ -0,0 +1,16 @@
+/*
+제목: fieldwidth
+기능: 필드 폭을 지정하여 정돈됭 출력
+파일 이름: fieldwidth.c
+수정날짜: 20210109
+작성자:zlzoneyub
+*/
+#include <stdio.h>  //헤더파일 선언
+
+int main(void)  //main 함수 시작
+{
+	printf("%-8s %-14s %5s \n", "이름", "학번", "학년");
+	printf("%-8s %-14s %5s \n", "zlzone", "202012345", "2");
+	return 0;
+}
+
